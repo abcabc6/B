667d0eaa9415ecf42c07648919ab46536a6ad95c
Xu Zhang <xuzhan@xuzhan.pek.csb>
2018-03-30 11:28:35 +0800

    My frist commit3

--- src/main/java/com/test/T1.java
+++ src/main/java/com/test/T1.java
@@ -25,7 +25,7 @@ public class T1 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: AAA").append(request.getContextPath());
-		System.out.println("iiiiiiiiiiiiiZZZZZZZZZZZZZZZ");
+		System.out.println("3333333333333iiiiiiiiiiiiiZZZZZZZZZZZZZZZ");
 	}
 
 	/**
